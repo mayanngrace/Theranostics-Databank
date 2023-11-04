8ed6fe7f658c93c63c2b1cf3edb2b99e5584d160
Max <mynngrc27@gmail.com>
2023-11-05 02:04:34 +0800

    max changes 2.5

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ]
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true 
+  }
 })
